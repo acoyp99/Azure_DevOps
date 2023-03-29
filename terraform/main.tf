a9cd1bf186b1f9e8537e8c8a7ffadaf98739a256
acoyp99 <1020840510@medellin.gov.co>
2023-03-28 23:45:49 -0500

    Terraform File

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,41 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    storage_account_name  = "__terraformstorageaccount__"
+    container_name        = "terraform"
+    key                   = "terraform.tfstate"
+    access_key            = "__storagekey__"
+  }
+  
 }
 
 # Configure the Microsoft Azure Provider
-
 provider "azurerm" {
   features {}
 }
 
+# Configure Azure Resource Group 
 resource "azurerm_resource_group" "rg" {
-  name     = "example-resources"
+  name     = "PULTerraform"
   location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "dev" {
+  name                = "__appserviceplan__"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+
+}
+
+resource "azurerm_app_service" "dev" {
+  name                = "__appservicename__"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
 }
